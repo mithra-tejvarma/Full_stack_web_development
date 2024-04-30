1e3fbd2e606d16f6f06f44f43c01708b33db888c
mithra-tejvarma <23701A05B8@aitsrajampet.ac.in>
2024-04-30 19:38:54 +0530

    second practise with arthemetic operators

--- javascript/01_practise.js
+++ javascript/01_practise.js
@@ -1,2 +1,4 @@
 console.log("Hello world");
 // this is the first practise file of java script
+//output:
+//Hello world
---
+++ javascript/02_practise.js
@@ -0,0 +1,17 @@
+let x = 20;//thid a number data type
+let y = 30.89;// this is also a number data type
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+x=5;
+y=5;
+console.log(x**y);
+/* output:
+50.89
+-10.89
+617.8
+0.6474587245063127
+20
+3125*/
