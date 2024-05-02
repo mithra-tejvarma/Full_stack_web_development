31312ae60c5c898d8b79317ff282519be3acba10
mithra-tejvarma <23701A05B8@aitsrajampet.ac.in>
2024-05-02 18:43:14 +0530

    third practise file

---
+++ javascript/03_practise.js
@@ -0,0 +1,12 @@
+//In this tutorial we are practising the differnt data types of javascript
+ 
+let x = 8;//number type
+
+let y = "Welcome";//string type
+
+let z = true ;//Bool type
+
+let a = Function
+
+console.log(typeof(a))//function type
+// and there are many more data types in javascript
